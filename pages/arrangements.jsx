1cd2812c61be66ab204eb61f4542e0a4b59f03f4
buckldav <runalldayandnight@gmail.com>
2021-11-22 22:52:46 -0700

    Scrape MuseScore URLs

--- pages/arrangements.jsx
+++ pages/arrangements.jsx
@@ -3,73 +3,63 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import Contact from '../components/Contact'
 import dynamic from 'next/dynamic';
+import puppeteer from 'puppeteer';
+
 const Layout = dynamic(() => import('../components/Layout'), {
   ssr: false
 });
 
-const MuseScoreUser = "buckldav";
-const MuseScoreURLs = [
-  {
-    "title": "Blue Christmas",
-    "embed": "https://musescore.com/user/30979026/scores/6399167/embed",
-    "url": "https://musescore.com/user/30979026/scores/6399167/s/_zAG0T"
-  },
-  {
-    "title": "Christmas Time is Here",
-    "embed": "https://musescore.com/user/30979026/scores/6405241/embed",
-    "url": "https://musescore.com/user/30979026/scores/6405241/s/6VxR4a"
-  },
-  {
-    "title": "Let It Snow",
-    "embed": "https://musescore.com/user/30979026/scores/5901967/embed",
-    "url": "https://musescore.com/user/30979026/scores/5901967/s/As2-ys"
-  },
-  // {
-  //   "title": "Sesame Street",
-  //   "embed": "https://musescore.com/user/30979026/scores/6465423/embed",
-  //   "url": "https://musescore.com/user/30979026/scores/6465423/s/48AEQR"
-  // },
-  // {
-  //   "title": "Spooky Scary Skeletons",
-  //   "embed": "https://musescore.com/user/30979026/scores/5901926/embed",
-  //   "url": "https://musescore.com/user/30979026/scores/5901926/s/As2-ys"
-  // },
-  {
-    "title": "O Come, O Come Emmanuel",
-    "embed": "https://musescore.com/user/30979026/scores/5901958/embed",
-    "url": "https://musescore.com/user/30979026/scores/5901958/s/As2-ys"
-  },
-  // {
-  //   "title": "Stayin' Alive",
-  //   "embed": "https://musescore.com/user/30979026/scores/5901929/embed",
-  //   "url": "https://musescore.com/user/30979026/scores/5901929/s/As2-ys"
-  // },
-  {
-    "title": "Santa Claus is Coming to Town",
-    "embed": "https://musescore.com/user/30979026/scores/7092626/embed",
-    "url": "https://musescore.com/user/30979026/scores/7092626/s/As2-ys"
-  },
-  {
-    "title": "The Christmas Song",
-    "embed": "https://musescore.com/user/30979026/scores/5901974/embed",
-    "url": "https://musescore.com/user/30979026/scores/5901974/s/As2-ys"
-  },
-  {
-    "title": "This Christmas",
-    "embed": "https://musescore.com/user/30979026/scores/6277403/embed",
-    "url": "https://musescore.com/user/30979026/scores/6277403/s/As2-ys"
-  },
-  {
-    "title": "Where Are You Christmas",
-    "embed": "https://musescore.com/user/30979026/scores/5901921/embed",
-    "url": "https://musescore.com/user/30979026/scores/5901921/s/As2-ys"
-  }    
-]
+const MuseScoreSets = {
+  "Christmas Arrangements": "https://musescore.com/user/30979026/sets/5111349",
+  "Jazz/Concert Band Arrangements": "https://musescore.com/user/30979026/sets/5111340",
+  "Jazz Standard Materials": "https://musescore.com/user/30979026/sets/5111343",
+  "Jazz Piano Materials": "https://musescore.com/user/30979026/sets/5111346"
+}
 
-const MusicArrangements = () => {
-  const [score, setScore] = useState(MuseScoreURLs[0])
+async function getMuseScoreSet(browser, url) {
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  // Wait for the scores to load and display the results.
+  const resultsSelector = 'article>div>div:nth-child(2)>a:nth-child(1)';
+  await page.waitForSelector(resultsSelector);
+
+  // Extract the results from the page.
+  return await page.evaluate((resultsSelector) => {
+    const anchors = Array.from(document.querySelectorAll(resultsSelector));
+    return anchors.map((anchor) => {
+      console.log(anchor.textContent, anchor.href)
+      return { title: anchor.textContent, embed: `${anchor.href}/embed`, url: anchor.href };
+    });
+  }, resultsSelector);
+}
+
+export async function getStaticProps() {
+  const browser = await puppeteer.launch();
+  const links = {}
+
+  for (const key of Object.keys(MuseScoreSets)) {
+    const msset = await getMuseScoreSet(browser, MuseScoreSets[key]); 
+    links[key] = msset.sort((a, b) => {
+      if(a.title < b.title) { return -1; }
+      if(a.title > b.title) { return 1; }
+      return 0;
+    })
+  }
+
+  await browser.close();
+  return {
+    props: { links },
+  }
+}
+
+const MusicArrangements = (props) => {
+  const [MSSet, setMSSet] = useState(Array.from(Object.keys(MuseScoreSets))[0])
+  const [score, setScore] = useState(props.links[Array.from(Object.keys(MuseScoreSets))[0]][0])
+  
   const changeActiveScore = (e) => {
-    setScore(MuseScoreURLs[e.target.id])
+    setScore(props.links[MSSet][e.target.id])
   }
 
   return (
@@ -95,24 +85,30 @@ const MusicArrangements = () => {
         </nav>
         <section className="grid-container margin-vertical-1">
           <div className="grid-y">
-            <div className="grid-x">
-              <div className="cell medium-6 padding-left-1">
-                <h5>Holiday Arrangements by <a target="_blank" rel="noopener noreferrer" href={"https://musescore.com/"+MuseScoreUser}>{MuseScoreUser}</a></h5>
-                <ul>
-                  {MuseScoreURLs.map((val, i) => (
-                    <li key={i}><button onClick={changeActiveScore} id={i} value={val.title} className="clear button padding-0 margin-0">{val.title}</button></li>
+            <p>
+              Here are some of my most recent arrangements. I specialize in religious/Christmas and jazz arranging. 
+              I charge around $15/part/minute for arrangements.<br/>Get in touch with me <a href="/arrangements#contact">here</a>!
+            </p>
+
+                <ul class="dropdown menu" data-dropdown-menu>
+                  <li>
+                    <a className="padding-left-0" href="#">Select Type of Arrangements</a>
+                    <ul class="menu" style={{width: 300}}>
+                      {Object.keys(MuseScoreSets).map(val => (
+                        <li><button className="clear button" onClick={() => { setMSSet(val); setScore(props.links[val][0]) }}>{val}</button></li>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+                <h5>{MSSet} by <a target="_blank" rel="noopener noreferrer" href="https://musescore.com/buckldav">buckldav</a></h5>
+                <ul className="grid-x margin-0 margin-bottom-1">
+                  {props.links[MSSet]?.map((val, i) => (
+                    <li className="display-inline-block margin-left-1 margin-right-1" key={i}><button onClick={changeActiveScore} id={i} value={val.title} className="clear button padding-0 margin-0">{val.title}</button></li>
                   ))}
                 </ul>
-              </div>
-              <div className="cell medium-6"> 
-                <p className="text-center">
-                  Here are some of my most recent arrangements. I specialize in religious/Christmas and jazz arranging. 
-                  I charge around $15/part/minute for arrangements.<br/>Get in touch with me <a href="/arrangements#contact">here</a>!
-                </p>
-              </div>
-            </div>
-            <iframe title={score.title} width="100%" height="600" src={score.embed} frameborder="0" allowfullscreen allow="autoplay; fullscreen"></iframe>
-            <span><a href={score.url} rel="noopener noreferrer" target="_blank">{score.title}</a> by <a href={"https://musescore.com/"+MuseScoreUser}>{MuseScoreUser}</a></span>
+
+            <iframe title={score.title} width="100%" height="600" src={score.embed} frameBorder="0" allow="autoplay; fullscreen"></iframe>
+            <span><a href={score.url} rel="noopener noreferrer" target="_blank">{score.title}</a> by <a href="https://musescore.com/buckldav">buckldav</a></span>
           </div>
         </section>
         <hr/>
